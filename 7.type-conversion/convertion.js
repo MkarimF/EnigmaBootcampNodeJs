5d74fe8d50b7f45eb5960348f4620698acd7d8f7
MkarimF <karimfadillah2001@gmail.com>
2024-12-30 20:06:06 +0700

    convertion

---
+++ 7.type-conversion/convertion.js
@@ -0,0 +1,18 @@
+let x = 5
+let y = 10
+
+x = x.toString()
+y = y.toString()
+
+let z = `${x} ${y}`
+console.log(z) // 510
+
+var enigma = "4";
+
+var bootCamp = 8;
+console.log(Number(enigma)+bootCamp);
+
+let clean = 3.14;
+
+
+console.log(Math.ceil(clean));

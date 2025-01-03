6e663d39ae2251a7a20c8795a035e3f85e3baa41
MkarimF <karimfadillah2001@gmail.com>
2025-01-03 16:36:44 +0700

    add function subject

---
+++ 12.function/function.js
@@ -0,0 +1,12 @@
+
+function segitiga (a) {
+  while (a<10) {
+    console.log("*".repeat(a))
+    a++
+    if (a==10) {
+      console.log("ini adalah bilangan terakhir")
+        }
+    }
+}
+
+segitiga(1)
